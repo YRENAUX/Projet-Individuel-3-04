cc690acf9fb1626043dfe20c78bb42c1fd0c8a68
YONI <yonirenaux0@gmail.com>
2019-04-03 10:42:31 +0200

    test

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Projet Individuel</title>
+    <meta charset="UTF-8">
+    <link href="css/style.css" rel="stylesheet" media="screen">
+    <script src="js/jquery.js" type="text/javascript" charset="utf-8"></script>
+    <link rel="stylesheet" href="css/prettyPhoto.css" type="text/css" media="screen" charset="utf-8" />
+    <script src="js/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
+</head>
+
+<body>
+    <?php include "header.html";?>
+    <div id="slider">
+        <figure>
+            <img src="img/galaxie1.jpg">
+            <img src="img/galaxie2.jpg">
+            <img src="img/galaxie3.jpg">
+    </div>
+    </div>
+    </figure>
+    <div class="container">
+        <img src="img/galaxie3.jpg">
+        <div class="paragraphe">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae quis debitis ullam laborum,
+                voluptatum iste natus cumque voluptatibus
+                eveniet ducimus voluptas sed vitae maiores tempora dolorem delectus, ratione totam nobis.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, qui dicta tempora, ipsa corporis error
+                explicabo voluptatum facere
+                debitis enim facilis quae officiis dignissimos non illo recusandae animi inventore similique?</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae quis debitis ullam laborum,
+                voluptatum iste natus cumque voluptatibus
+                eveniet ducimus voluptas sed vitae maiores tempora dolorem delectus, ratione totam nobis.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, qui dicta tempora, ipsa corporis error
+                explicabo voluptatum facere
+                debitis enim facilis quae officiis dignissimos non illo recusandae animi inventore similique?</p>
+        </div>
+    </div>
+    <div class="paralax">
+    </div>
+    <div class="gallery">
+        <img src="img/img_forest.jpg" width="600" height="400">
+        <div class="desc">Add a description of the image here</div>
+    </div>
+    <div class="gallery">
+        <img src="img/img_forest.jpg" width="600" height="400">
+        <div class="desc">Add a description of the image here</div>
+    </div>
+    <div class="gallery">
+        <img src="img/img_forest.jpg" width="600" height="400">
+        <div class="desc">Add a description of the image here</div>
+    </div>
+    <div class="gallery">
+        <img src="img/img_forest.jpg" width="600" height="400">
+        <div class="desc">Add a description of the image here</div>
+    </div>
+    <div class="gallery2">
+        <img src="img/img_forest.jpg" width="600" height="400">
+        <div class="desc">Add a description of the image here</div>
+    </div>
+    <div class="gallery2">
+        <img src="img/img_forest.jpg" width="600" height="400">
+        <div class="desc">Add a description of the image here</div>
+    </div>
+    <div class="gallery2">
+        <img src="img/img_forest.jpg" width="600" height="400">
+        <div class="desc">Add a description of the image here</div>
+    </div>
+    <div class="gallery2">
+        <img src="img/img_forest.jpg" width="600" height="400">
+        <div class="desc">Add a description of the image here</div>
+    </div>
+    <div class="gallery2">
+        <img src="img/img_forest.jpg" width="600" height="400">
+        <div class="desc">Add a description of the image here</div>
+    </div>
+    <div class="gallery3">
+        <a href="img/img_forest.jpg" rel="prettyPhoto[pp_gal]" title="You can add caption to pictures."><img
+                src="img/img_forest.jpg" width="600" height="400" alt="Red round shape" /></a>
+
+        <a href="img/img_forest.jpg" rel="prettyPhoto[pp_gal]"><img src="img/img_forest.jpg" width="600" height="400"
+                alt="Nice building" /></a>
+
+        <a href="img/img_forest.jpg" rel="prettyPhoto[pp_gal]"><img src="img/img_forest.jpg" width="600" height="400"
+                alt="Fire!" /></a>
+
+        <a href="img/img_forest.jpg" rel="prettyPhoto[pp_gal]"><img src="img/img_forest.jpg" width="600" height="400"
+                alt="Rock climbing" /></a>
+
+        <a href="img/img_forest.jpg" rel="prettyPhoto[pp_gal]"><img src="img/img_forest.jpg" width="600" height="400"
+                alt="Fly kite, fly!" /></a>
+        <a href="img/img_forest.jpg" rel="prettyPhoto[pp_gal]"><img src="img/img_forest.jpg" width="600" height="400"
+                alt="Fly kite, fly!" /></a>
+    </div>
+    <?php include "footer.html"; ?>
+</body>
